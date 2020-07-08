e97fc344d011b02756ecb92656df29137d15fbc2
hemanth kumar <hemanth.sakamuri@imaginate.in>
2020-07-08 19:01:35 +0530

    theme reference in app.js change

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import Theme from "../src/ui/theme";
+import Theme from "../src/ui/Theme";
 
 export default class MyDocument extends Document {
   render() {
